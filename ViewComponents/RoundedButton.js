80e1abe98d90c481e7f3ebbc1299ff196fd11d39
Jageloo <jageloo.yadav@gmail.com>
2022-02-22 02:02:43 +0530

    Rounded button component added

--- ViewComponents/RoundedButton.js
+++ ViewComponents/RoundedButton.js
@@ -6,23 +6,30 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 export class RoundedButton extends React.Component {
     render() {
         return (
-                <View style={styles.button}>
-                <Button title = {this.props.title} color = "red" />
+                <View style={ this.props.buttonStyle == "primary" ? styles.primaryButton : styles.secondaryButton }>
+                <Button title = {this.props.title}
+                color = {this.props.buttonStyle == "primary" ? "white" : "#ED3F6E"}
+                />
                 </View>
                 );
     }
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1
-   },
-   button: {
-      backgroundColor: '#00aeef',
-      borderColor: 'red',
+   primaryButton: {
+      backgroundColor: '#ED3F6E',
+      borderColor: '#ED3F6E',
       borderWidth: 0,
-      borderRadius: 20
-   }
+      borderRadius: 24.0,
+      height: 48.0
+   },
+   secondaryButton: {
+   backgroundColor: 'white',
+   borderColor: '#ED3F6E',
+   borderWidth: 1.0,
+   borderRadius: 24.0,
+   height: 48.0
+}
 })
 
 
